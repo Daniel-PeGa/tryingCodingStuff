661cc8d7da41dcb2dc130a26225a059788f75a31
Daniel-PeGa <danielperezgarnic@gmail.com>
2023-02-06 16:21:05 -0600

    ya sirve la llamada, ya nomas falta el color

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-const inputText = document.querySelector('#txt');
 const myButton = document.querySelector('.btn-list');
 const list = document.querySelector('.container ul');
 
 myButton.addEventListener('click', (e)=>{
-    // if(inputText.value != ""){
-        // e.preventDefault();
-    //     // create li
-    //     const myLi = document.createElement('li');
-    //     myLi.innerHTML = inputText.value;
-    //     list.appendChild(myLi);
 
-    //     // create a span
-    //     const mySpan = document.createElement('span');
-    //     mySpan.innerHTML = 'x';
-    //     myLi.appendChild(mySpan);
-    // }
-    // const close = document.querySelectorAll('span');
-    // for(let i=0; i<close.length; i++){
-    //     close[i].addEventListener('click', ()=>{
-    //         close[i].parentElement.style.opacity = 0;
-    //         setTimeout(() => {
-                
-    //             close[i].parentElement.style.display = "none";
-    //             close[i].parentElement.remove();
-    //         }, 500);
-    //     })
-    // }
-    // inputText.value = "";
     var employeeName = prompt("What is your employee's name?");
     var employeeProfession = prompt("What does the employee do?");
     var employeeNumber = prompt("What is the new employee's phone number?");
@@ -37,7 +13,7 @@ myButton.addEventListener('click', (e)=>{
     <h1>${employeeName}</h1>
     <p>${employeeProfession}</p>
     <div class="phone">
-    <p><a href="tel: ${employeeNumber}">${employeeNumber}</a></p>
+    <p><a href="tel: ${employeeNumber}" class="no-decoration">${employeeNumber}</a></p>
     </div>
     `;
     list.appendChild(myLi);
