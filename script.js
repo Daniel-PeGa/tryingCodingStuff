1a7490b9f6735c67190d64743ea1838c3ed01047
Daniel-PeGa <danielperezgarnic@gmail.com>
2023-02-12 13:53:40 -0600

    idk how tf to make this work!!!!

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var employees = [];
 const addEmployee = (ev)=>{
     ev.preventDefault();  //to stop the form submitting
     let employee = {
-        id: document.getElementById('id').value,
+        id: Date.now(),
         name: document.getElementById('name').value,
         position: document.getElementById('position').value,
         phone: document.getElementById('phoneNumber').value,
@@ -15,15 +15,20 @@ document.forms[0].reset(); // to clear the form for the next entry
 
 console.warn('added' , {employees} );
 let pre = document.querySelector('#msg pre');
-pre.textContent = '\n' + JSON.stringify(employees, '\t', 5);
+pre.textContent = JSON.stringify(employees, '\t', 3);
 
 //saving to localStorage
 localStorage.setItem('MyEmployeeList', JSON.stringify(employees) );
 };
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('submit').addEventListener('click', addEmployee);
-});
+// document.addEventListener('DOMContentLoaded', ()=>{
+//     document.getElementById('submit').addEventListener('click', addEmployee);
+// });
+
+document.getElementById('msg').innerHTML = localStorage.getItem('MyEmployeeList');
 
 
 //  Andabamos viendo este video: https://www.youtube.com/watch?v=NxVCq4p0Kb0 pa guiarnos con este nuevo proyecto para guardar los elementos en el local Storage
+
+
+
